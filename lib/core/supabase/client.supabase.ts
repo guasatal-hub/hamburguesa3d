f62f71bca35e7b958d301ac1148222dee0d3ddbc
guasatald <guasatald@gmail.com>
2026-02-18 16:52:51 +0000

    📁: Creacion de archivos/carpetas - se creo el archivo .env

--- lib/core/supabase/client.supabase.ts
+++ lib/core/supabase/client.supabase.ts
@@ -1,14 +1,18 @@
 import { createClient } from '@supabase/supabase-js';
 import { storageAdapter } from '../storage/storge.adapter';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://oanerhrcrlyaoxpajglh.supabase.co';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hbmVyaHJjcmx5YW94cGFqZ2xoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNzgxMDEsImV4cCI6MjA4Njk1NDEwMX0.BoL1xzCAIVpsqmRQ56hlLdcOmXsRdkGPi3TRCXyNIOI';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase environment variables are missing');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-        storage: storageAdapter,
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: false,
-    },
+  auth: {
+    storage: storageAdapter,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
 });
